c0a6cb72875937bac181230b7e18736fa26e1f4c
Lucas da Silva Carnelosi <110860654+lucascarnelosi@users.noreply.github.com>
2023-08-23 17:48:16 -0300

    first commit
    creating the project

---
+++ src/js/index.js
@@ -0,0 +1,54 @@
+const result = document.querySelector('.result-number');
+const resetKey = document.getElementById('reset');
+const numbers = document.querySelectorAll('.number');
+const moreLess = document.querySelector('.more-less');
+const operators = document.querySelectorAll('.op');
+const equalSign = document.getElementById('equal-sign');
+const point = document.getElementById('dot')
+
+function adicionarDigito(digit) {
+    if (typeof(digit) == 'string') {
+        result.innerHTML += digit
+        if (result.innerHTML.includes('..')) {
+            result.innerHTML = 0
+        }
+    } else if (result.innerHTML == 0) {
+        result.innerHTML = digit
+    } else {
+        result.innerHTML += digit
+    }
+}
+
+function trocaSinal() {
+    if (result.innerHTML.includes('-')) {
+        result.innerHTML += ''
+    } else {
+        result.innerHTML = `-${result.innerHTML}`
+    }
+}
+
+resetKey.addEventListener('click', () => result.innerHTML = 0)
+
+moreLess.addEventListener('click', () => {
+    trocaSinal()
+})
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        adicionarDigito(Number(number.innerHTML))        
+    })
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        adicionarDigito(operator.innerHTML)
+    })
+})
+
+point.addEventListener('click', () => {
+    adicionarDigito(point.innerHTML)
+})
+
+equalSign.addEventListener('click', () => {
+    result.innerHTML = eval(result.innerHTML)
+})
